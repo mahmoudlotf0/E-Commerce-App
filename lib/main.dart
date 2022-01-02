6a9cf9585cf932438b94d1c816ba60089caeb37c
mahmoudlotf0 <lotfymahmoud07@outlook.com>
2022-01-02 14:36:09 +0200

    Add constans folder

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShopApp());
+}
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
