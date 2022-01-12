664cd7dc821791f530f1a933102e9822ef6ea8d8
mahmoudlotf0 <lotfymahmoud07@outlook.com>
2022-01-12 13:48:03 +0200

    Add search field in header of product screen

---
+++ lib/presentationLayer/screens/products_screen/components/search_product.dart
@@ -0,0 +1,37 @@
+import 'package:ecommerceapp/constans/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SearchProduct extends StatelessWidget {
+  const SearchProduct({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController searchController = TextEditingController();
+
+    return Container(
+      width: SizeConfig.screenWidth * 0.6,
+      height: getProportionateScreenHeight(50),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: TextField(
+        controller: searchController,
+        cursorColor: Colors.grey[700],
+        decoration: InputDecoration(
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          prefixIcon: const Icon(
+            Icons.search,
+            color: Colors.grey,
+          ),
+          hintText: 'Search Product',
+          hintStyle: GoogleFonts.muli(
+            textStyle: const TextStyle(color: Colors.grey),
+          ),
+        ),
+      ),
+    );
+  }
+}
